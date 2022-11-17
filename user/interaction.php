c6f46e184f1e361f2016cbe8fb5f9d6b28ad1b48
farex4sure <faruqhassan176@gmail.com>
2022-11-17 12:52:38 +0100

    farex update

--- user/interaction.php
+++ user/interaction.php
@@ -1,4 +1,19 @@
-         =>  'hour',
+<?php
+session_start();
+include "config.php";
+$list="";
+if(!isset($_SESSION['loggedin_user'])){
+    header("location:signin.php");
+}
+function get_time_ago( $time )
+{
+    $time_difference = time() - $time;
+
+    if( $time_difference < 1 ) { return 'Just now'; }
+    $condition = array( 12 * 30 * 24 * 60 * 60 =>  'year',
+                30 * 24 * 60 * 60       =>  'month',
+                24 * 60 * 60            =>  'day',
+                60 * 60                 =>  'hour',
                 60                      =>  'minute',
                 1                       =>  'second'
     );
