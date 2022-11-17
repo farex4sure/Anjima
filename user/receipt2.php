1d2d335e80316c472e4cefc382c38991e5a31179
farex4sure <faruqhassan176@gmail.com>
2022-11-17 12:54:17 +0100

    farex update

--- user/receipt2.php
+++ user/receipt2.php
@@ -1,4 +1,16 @@
-UA-Compatible" content="IE=edge">
+<?php
+session_start();
+include "config.php";
+if(!isset($_SESSION['loggedin_user'])){
+    header("location:signin.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="js/jquery.min.js"></script> 
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Anjima | Receipt</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
